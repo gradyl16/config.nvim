df959660ff6be2346a1cd7422d869bace241b91c
Dylen Greenenwald <gradyl16@protonmail.com>
2024-10-13 18:29:58 -0500

    feat: lint/format build routines

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ If you are experiencing issues, please make sure you have the latest versions.
 ### Install External Dependencies
 
 External Requirements:
+
 - Basic utils: `git`, `make`, `unzip`, C Compiler (`gcc`)
 - [ripgrep](https://github.com/BurntSushi/ripgrep#installation)
 - Clipboard tool (xclip/xsel/win32yank or other depending on platform)
--- lua/dylen/plugins/conform.lua
+++ lua/dylen/plugins/conform.lua
@@ -28,12 +28,17 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        markdown = { 'prettier' },
+
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        -- javascript = { { 'prettierd', 'prettier' } },
+      },
+      linters_by_ft = {
+        markdown = { 'markdownlint' },
       },
     },
   },
